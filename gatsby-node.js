34fea096918b934af9f7ac926a6ba4d3cf75400a
Labi Francis <moodyblerd@gmail.com>
2022-11-28 16:54:29 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,9 @@
-
+// gatsby-node.js
 exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
-createRedirect({
-  fromPath: `/page-2/*`,
-  toPath: `https://www.gatsbyjs.com/docs/*`,
-  statusCode: 200,
-})
+		
+	createRedirect({
+    fromPath: `/blog/recipes/mouthwatering-lasagna`,
+    toPath: `/recipes/mouthwatering-lasagna`,
+  });
+}
