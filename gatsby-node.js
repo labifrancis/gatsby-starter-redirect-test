1a83e19c6bbeb24425ca779b0235fe60845cfa9d
Labi Francis <moodyblerd@gmail.com>
2022-11-28 16:50:01 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-
 
 exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
